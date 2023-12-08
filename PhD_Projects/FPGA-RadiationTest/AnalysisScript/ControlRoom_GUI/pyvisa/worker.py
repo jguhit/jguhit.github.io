a5cb3399cabd76b5512c2e9fe5abade6c66abcef
Jem Aizen Guhit <guhitj@phys-guhitj-mbp.local>
2023-12-08 10:31:03 -0500

    Fix Folders

--- PhD_Projects/FPGA-RadiationTest/AnalysisScript/ControlRoom_GUI/pyvisa/worker.py
+++
@@ -1,7 +0,0 @@
-from PyQt5.QtCore import QThread, QObject, pyqtSignal, pyqtSlot
-import time 
-
-class Worker(QObject):
-    finished = pyqtSignal()
-
-
--- PhD_Projects/NetBASILISK/Benchmark_Scripts/Scripts/PP_AGLT2_Metrics.py
+++
@@ -1,7 +0,0 @@
-from PP_AGLT2_func import AGLT2_Stats
-import time
-
-### AGLT2 ###
-metadata = ['CPU_load', 'CPU_utilization', 'Disk_IO_SUMMARY', 'Memory']
-for j in metadata:
-        AGLT2_Stats('AGLT2', '{}'.format(j))
